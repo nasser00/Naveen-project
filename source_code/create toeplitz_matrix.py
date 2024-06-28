f0ae54509714952850245dd75513d62f6199cfee
NAVEEN8708 <138665871+NAVEEN8708@users.noreply.github.com>
2024-06-28 11:16:39 -0400

    Create create toeplitz_matrix.py

---
+++ source_code/create toeplitz_matrix.py
@@ -0,0 +1,15 @@
+def toep(p,wo):
+    """
+    input
+    p=length of data vector
+    wo= initial wavelet vector
+    output
+    A=toeplitz matrix of  sorce wavelet
+    """
+    q=len(wo)
+    col=np.hstack((wo,np.zeros(p-q)))  #first column and first row of toeplitz matr
+    row=np.hstack((wo[0],np.zeros(p-q)))
+    from scipy.linalg import toeplitz
+    A=toeplitz(col,row)
+    return(A)
+A=toep(p,y)
