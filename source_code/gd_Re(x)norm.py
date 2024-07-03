d135a93b2c3aa32ea3401baf38791e086e7d54c4
NAVEEN8708 <138665871+NAVEEN8708@users.noreply.github.com>
2024-07-02 20:33:50 -0400

    Update gd_Re(x)norm.py

--- source_code/gd_Re(x)norm.py
+++ source_code/gd_Re(x)norm.py
@@ -1,14 +1,31 @@
 def decon_reg_new(A,b,xo,my_lambda,Max_iter):
+     """
+    input
+    A=source wavelet matrix
+    b= data vector
+    my_lambda=regularization parameter
+    xo=initial guess
+    Max_iter= no of iteration
+    
+    output
+    x_sol=solution of our data
+    final_iter=no of iterations taken 
+    
+    
+    """
     i=0
     while i<Max_iter: 
         e=0.000001
         temp=(np.dot(A,xo)-b)
-        r=np.dot(A.T,temp) +(my_lambda/2)*(np.divide(xo,(np.sqrt((xo)**2+e**2))))
-        beta=0.1;#np.dot(r.T,r)/np.dot(r.T,np.dot(A.T,b))
+        #use Re(x)norm in regularization term insted of L2 norm 
+        #Re(x)=sqrt(x^2+e^2)-e
+        #derivative of Re(x)=x/sqrt(x^2+e^2)
+        r=np.dot(A.T,temp) +(my_lambda/2)*(np.divide(xo,(np.sqrt((xo)**2+e**2)))) 
+        beta=0.1;#learning rate for algorithm or np.dot(r.T,r)/np.dot(r.T,np.dot(A.T,b))
         y=xo
-        xo=xo-beta*r
+        xo=xo-beta*r #updating solution 
         xnew=xo
-        if np.linalg.norm(xnew-y)<0.0001:
+        if np.linalg.norm(xnew-y)<0.0001: #stopping criteria
             break
         else:
             i=i+1
@@ -17,5 +34,3 @@ def decon_reg_new(A,b,xo,my_lambda,Max_iter):
     final_iter=i;
 
     return x_sol,final_iter
-# x_sol_reg_new,final_iter=decon_reg_new(A,dnew,xo,0,500)
-# plt.plot(x_sol_reg_new)
