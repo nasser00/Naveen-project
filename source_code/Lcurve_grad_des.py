f9c6aec1fada3434d5694802d03ef0577427cde6
NAVEEN8708 <138665871+NAVEEN8708@users.noreply.github.com>
2024-07-02 20:24:41 -0400

    Update Lcurve_grad_des.py

--- source_code/Lcurve_grad_des.py
+++ source_code/Lcurve_grad_des.py
@@ -3,17 +3,18 @@ x=[]  #empty list for solution  norm
 y=[]   #empty list for residual norm
 for i in my_lambda:
     x_sol,itr=decon_reg(A,dnew,xo,i,500)
-    x.append((np.linalg.norm(x_sol))**2)
-    y.append((np.linalg.norm(dnew-np.dot(A,x_sol)))**2)
+    x.append((np.linalg.norm(x_sol))**2) #calculating norm of solution 
+    y.append((np.linalg.norm(dnew-np.dot(A,x_sol)))**2)#calculating norm of residual  
 plt.figure(figsize=(8, 4))
-plt.title('L curve')
+plt.title('L curve gradient descent')
 plt.ylabel("residual norm")
 plt.xlabel("solution norm ")
 plt.grid(True) 
 plt.plot(x,y,".-")
 for i in range(len(x)):
     plt.text(x[i],y[i],""+str(np.round(my_lambda[i],2)),fontsize=6)
+# saving the L curve     
 directory = './results/noisy_data'
 figure_path = os.path.join(directory,'L_curve.png')
 plt.savefig(figure_path)
-plt.show()  
+plt.show()    
