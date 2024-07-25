fe3d2bc45d2bde9ca80ee0cf1af066b447fd744e
NAVEEN8708 <138665871+NAVEEN8708@users.noreply.github.com>
2024-07-24 20:04:55 -0400

    Add files via upload

--- source_code/cauchy.py
+++ source_code/cauchy.py
@@ -1,19 +1,29 @@
-def cauchy(A,b,xo,my_lambda,Max_iter):
+def cauchy(A,b,xo,my_lambda,Max_iter,a):
+    """
+    A=source wavelet matrix
+    b= data vector
+    my_lambda=regularization parameter
+    xo=initial guess
+    Max_iter= no of iteration
+    epsilon =thresholding parameter  
+    output
+    x_sol=solution of our data
+    final_iter=no of iterations taken 
+    """
+    import numpy as np 
     i=0
-    import numpy as np
     while i<Max_iter: 
         temp=(np.dot(A,xo)-b)
-        r=np.dot(A.T,temp) +(my_lambda/2)*(np.divide(xo,((xo**2/2)+1)))
+        r=np.dot(A.T,temp) +(my_lambda)*((2*xo)/(xo**2+a))
         beta=0.005;#np.dot(r.T,r)/np.dot(r.T,np.dot(A.T,b))
         y=xo
         xo=xo-beta*r
-        xnew=xo
-        if np.linalg.norm(xnew-y)<0.0001:
+        if np.linalg.norm(r)<0.0000001:
             break
         else:
             i=i+1
             
-    x_sol=xnew;
+    x_sol=xo;
     final_iter=i;
 
     return x_sol,final_iter
